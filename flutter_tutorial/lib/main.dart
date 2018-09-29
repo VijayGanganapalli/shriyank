a51f9db5ea2dcff0972d50bb0f60df60dbc2c469
Vijay <vijayganganapalli@gmail.com>
2018-09-29 12:49:50 +0530

    simplify the code and refactoring

--- flutter_tutorial/lib/main.dart
+++ flutter_tutorial/lib/main.dart
@@ -1,27 +1,21 @@
 //TODO: import librarary
 import 'package:flutter/material.dart';
+import 'app_screens/first_screen.dart';
 
 //TODO: create main method
-void main() {
-  //TODO: run your app here
-  runApp(
-    MaterialApp(
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
       title: "Flutter Tutorial",
       home: Scaffold(
         appBar: AppBar(
           title: Text("Flutter Tutorial"),
         ),
-        body: Material(
-          color: Colors.lightBlueAccent,
-          child: Center(
-            child: Text(
-              "Hello flutter",
-              textDirection: TextDirection.ltr,
-              style: TextStyle(color: Colors.white, fontSize: 40.0),
-            ),
-          ),
-        ),
+        body: FirstScreen(),
       ),
-    ),
-  );
+    );
+  }
 }
