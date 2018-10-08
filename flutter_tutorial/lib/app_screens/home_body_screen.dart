dde9eb721f1884b3f550a7c2dc554adbe544d4fc
Vijay <vijayganganapalli@gmail.com>
2018-10-08 17:20:49 +0530

    refactoring with methods

--- flutter_tutorial/lib/app_screens/home_body_screen.dart
+++ flutter_tutorial/lib/app_screens/home_body_screen.dart
@@ -12,87 +12,19 @@ class HomeBodyScreen extends StatelessWidget {
           children: <Widget>[
             Row(
               children: <Widget>[
-                Expanded(
-                  child: Text(
-                    "Spice Jet",
-                    textDirection: TextDirection.ltr,
-                    style: TextStyle(
-                      color: Colors.white,
-                      fontSize: 35.0,
-                      fontFamily: 'Ralewway',
-                      fontWeight: FontWeight.w700,
-                    ),
-                  ),
-                ),
-                Expanded(
-                  child: Text(
-                    "From Delhi to Bangalore",
-                    textDirection: TextDirection.ltr,
-                    style: TextStyle(
-                      color: Colors.white,
-                      fontSize: 20.0,
-                      fontFamily: 'Ralewway',
-                      fontWeight: FontWeight.w700,
-                    ),
-                  ),
-                ),
+                buildFlightTitle("Spice Jet"),
+                buildFlightDesc("From Delhi to Bangalore via Mumbai"),
               ],
             ),
             SizedBox(height: 10.0),
             Row(
               children: <Widget>[
-                Expanded(
-                  child: Text(
-                    "Air India",
-                    textDirection: TextDirection.ltr,
-                    style: TextStyle(
-                      color: Colors.white,
-                      fontSize: 35.0,
-                      fontFamily: 'Ralewway',
-                      fontWeight: FontWeight.w700,
-                    ),
-                  ),
-                ),
-                Expanded(
-                  child: Text(
-                    "From Bangalore to Goa via Mumbai",
-                    textDirection: TextDirection.ltr,
-                    style: TextStyle(
-                      color: Colors.white,
-                      fontSize: 20.0,
-                      fontFamily: 'Ralewway',
-                      fontWeight: FontWeight.w700,
-                    ),
-                  ),
-                ),
+                buildFlightTitle("Air India"),
+                buildFlightDesc("From Hyderabad to Goa via Bangalore"),
               ],
             ),
-            Container(
-              alignment: Alignment.center,
-              child: Image.asset(
-                "images/flight.png",
-                height: 200.0,
-                width: 250.0,
-              ),
-            ),
-            Container(
-              margin: EdgeInsets.only(top: 30.0),
-              width: 200.0,
-              height: 50.0,
-              child: RaisedButton(
-                color: Colors.deepOrange,
-                onPressed: () => buildFligtAlertDialog(context),
-                child: Text(
-                  "Book your flight",
-                  style: TextStyle(
-                      color: Colors.white,
-                      fontSize: 20.0,
-                      fontFamily: "Raleway",
-                      fontWeight: FontWeight.w700),
-                ),
-                elevation: 6.0,
-              ),
-            ),
+            buildFlightImage(),
+            buildFlightBookButton(context),
           ],
         ),
       ),
@@ -109,6 +41,68 @@ class HomeBodyScreen extends StatelessWidget {
           content: Text("Have a pleasant fligt"),
         );
       },
+    );
+  }
+
+  Widget buildFlightTitle(String title) {
+    return Expanded(
+      child: Text(
+        title,
+        textDirection: TextDirection.ltr,
+        style: TextStyle(
+          color: Colors.white,
+          fontSize: 35.0,
+          fontFamily: 'Ralewway',
+          fontWeight: FontWeight.w700,
+        ),
+      ),
+    );
+  }
+
+  Widget buildFlightDesc(String desc) {
+    return Expanded(
+      child: Text(
+        desc,
+        textDirection: TextDirection.ltr,
+        style: TextStyle(
+          color: Colors.white,
+          fontSize: 20.0,
+          fontFamily: 'Ralewway',
+          fontWeight: FontWeight.w700,
+        ),
+      ),
+    );
+  }
+
+  Widget buildFlightImage() {
+    return Container(
+      alignment: Alignment.center,
+      child: Image.asset(
+        "images/flight.png",
+        height: 200.0,
+        width: 250.0,
+      ),
+    );
+  }
+
+  Widget buildFlightBookButton(context) {
+    return Container(
+      margin: EdgeInsets.only(top: 30.0),
+      width: 200.0,
+      height: 50.0,
+      child: RaisedButton(
+        color: Colors.deepOrange,
+        onPressed: () => buildFligtAlertDialog(context),
+        child: Text(
+          "Book your flight",
+          style: TextStyle(
+              color: Colors.white,
+              fontSize: 20.0,
+              fontFamily: "Raleway",
+              fontWeight: FontWeight.w700),
+        ),
+        elevation: 6.0,
+      ),
     );
   }
 }
