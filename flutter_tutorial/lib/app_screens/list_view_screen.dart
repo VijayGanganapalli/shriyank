3f8100ebb597d0c094615a62ac80e3da61a410fe
Vijay <vijayganganapalli@gmail.com>
2018-10-08 18:24:06 +0530

    added long listview

--- flutter_tutorial/lib/app_screens/list_view_screen.dart
+++ flutter_tutorial/lib/app_screens/list_view_screen.dart
@@ -1,31 +1,23 @@
 import 'package:flutter/material.dart';
 
 class ListViewScreen extends StatelessWidget {
+  final listItems = getListElements();
+
   @override
   Widget build(BuildContext context) {
-    return ListView(
-      children: <Widget>[
-        ListTile(
-          leading: Icon(Icons.landscape),
-          title: Text("Landscape"),
-          subtitle: Text("Beautiful view !"),
-          trailing: Icon(Icons.wb_sunny),
-          onTap: () => debugPrint("Landscape tapped !"),
-        ),
-        ListTile(
-          leading: Icon(Icons.laptop_windows),
-          title: Text("Windows"),
-        ),
-        ListTile(
-          leading: Icon(Icons.phone),
-          title: Text("Phone"),
-        ),
-        Text("Yet another element in List"),
-        Container(
-          color: Colors.red,
-          height: 50.0,
-        ),
-      ],
+    return ListView.builder(
+      itemBuilder: (context, index) {
+        return ListTile(
+          leading: Icon(Icons.arrow_right),
+          title: Text(listItems[index]),
+          onTap: () => debugPrint("${listItems[index]} was tapped"),
+        );
+      },
     );
   }
+}
+
+List<String> getListElements() {
+  var items = List.generate(100, (index) => "Item $index");
+  return items;
 }
